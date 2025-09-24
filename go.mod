d1ed68a7de99b5c4708b64ac32fedd69f10e4c6d
Joshua Emralino <joshua.s.emralino@gmail.com>
2025-09-24 16:08:42 -0700

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JoshuaSE-git/snippetbox
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+
+	log.Print("starting server on http://localhost:4000")
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
