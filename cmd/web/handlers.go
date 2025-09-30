2faa8ae0a43b2c1b29724e0c0e927dbd0bee5e2a
JoshuaSE-git <joshua.s.emralino@gmail.com>
2025-09-29 18:54:04 -0700

    added list operation to snippets model

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
 
@@ -12,23 +11,31 @@ import (
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
-	files := []string{
-		"./ui/html/base.tmpl.html",
-		"./ui/html/pages/home.tmpl.html",
-		"./ui/html/partials/nav.tmpl.html",
-	}
-
-	ts, err := template.ParseFiles(files...)
+	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
 
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		app.serverError(w, r, err)
-		return
-	}
+	fmt.Fprintf(w, "%+v\n", snippets)
+
+	//files := []string{
+	//	"./ui/html/base.tmpl.html",
+	//	"./ui/html/pages/home.tmpl.html",
+	//	"./ui/html/partials/nav.tmpl.html",
+	//}
+
+	//ts, err := template.ParseFiles(files...)
+	//if err != nil {
+	//	app.serverError(w, r, err)
+	//	return
+	//}
+
+	//err = ts.ExecuteTemplate(w, "base", nil)
+	//if err != nil {
+	//	app.serverError(w, r, err)
+	//	return
+	//}
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
